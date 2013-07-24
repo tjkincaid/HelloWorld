b78a6a9b63bdaeeb30d3d6b0bdc7249a649e669e
TJ Kincaid <admin@TJs-MacBook-Pro.local>
2013-07-23 21:33:14 -0400

    Initial Commit

---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by TJ Kincaid on 7/23/13.
+//  Copyright (c) 2013 TJ Kincaid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
---
+++ HelloWorld/HelloWorldViewController.m
@@ -0,0 +1,29 @@
+//
+//  HelloWorldViewController.m
+//  HelloWorld
+//
+//  Created by TJ Kincaid on 7/23/13.
+//  Copyright (c) 2013 TJ Kincaid. All rights reserved.
+//
+
+#import "HelloWorldViewController.h"
+
+@interface HelloWorldViewController ()
+
+@end
+
+@implementation HelloWorldViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
