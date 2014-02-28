4bea01f4dc4a056d72b87a9c547ab780c0b48a27
TJ Kincaid <tjkincaid92@gmail.com>
2014-02-28 18:54:04 -0500

    Initial Commit

---
+++ HelloWorld/HelloWorld/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  HelloWorldAppController.h
+//  HelloWorld
+//
+//  Created by TJ Kincaid on 2/28/14.
+//  Copyright __MyCompanyName__ 2014. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ HelloWorld/HelloWorld/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  HelloWorldAppController.h
+//  HelloWorld
+//
+//  Created by TJ Kincaid on 2/28/14.
+//  Copyright __MyCompanyName__ 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
